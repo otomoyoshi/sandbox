7d8edb6460a2a548c900a0a87014de5c2f6ccefe
otomoyoshi <tom@onotomoyoshinoMacBook-Pro.local>
2018-12-05 23:58:24 +0900

    try to use stream api

--- stream_api.java
+++ stream_api.java
@@ -1,5 +1,8 @@
 import java.util.List;
+import java.util.stream.Stream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 class StreamApi {
     public static void main(String args[]){
@@ -16,8 +19,20 @@ class StreamApi {
             Integer number = Integer.valueOf((int) Math.round(Math.random() * MAX));
             list.add(number);
         }
-        for(Integer num: list){
-            System.out.print(num + ",");
-        }
+        // for(Integer num: list){
+        //     System.out.print(num + ",");
+        // }
+
+        // Stream<Integer> stream = list.stream();
+        // stream.forEach(System.out::println);
+
+        Stream<Integer> stream = list.stream()
+            .filter(i -> i%2 == 0)
+            // .sorted(Comparator.reverseOrder())
+            .sorted()
+            .distinct();
+        System.out.println();
+        stream.forEach(System.out::println);
+
     }
 }
